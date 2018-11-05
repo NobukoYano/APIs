59c702c23851adcb9d95fae6160ed218e2515d6c
NobukoYano <santorini0831@gmail.com>
2018-11-05 20:31:37 +0100

    create Lesson_3/06 Adding Features to your Mashup

--- Lesson_3/06_Adding Features to your Mashup/Starter Code/views.py
+++ Lesson_3/06_Adding Features to your Mashup/Starter Code/views.py
@@ -1,4 +1,4 @@
-from findARestaurant import findARestaurant
+from findARestaurant_bk import findARestaurant
 from models import Base, Restaurant
 from flask import Flask, jsonify, request
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,36 +7,79 @@ from sqlalchemy import create_engine
 
 import sys
 import codecs
-sys.stdout = codecs.getwriter('utf8')(sys.stdout)
-sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 
+# sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+# sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 
+# foursquare_client_id = environ.get('foursquare_clinet_id')
+# foursquare_client_secret = environ.get('foursquare_client_secret')
+# google_api_key = environ.get('google_api_key')
 
-
-#foursquare_client_id = ''
-
-#foursquare_client_secret = ''
-
-#google_api_key = ''
-
-engine = create_engine('sqlite:///restaruants.db')
+# engine = create_engine('sqlite:///restaurants.db')
+engine = create_engine('sqlite:///restaruants.db?check_same_thread=False')
 
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 app = Flask(__name__)
 
-@app.route('/restaurants', methods = ['GET', 'POST'])
+
+@app.route('/restaurants', methods=['GET', 'POST'])
 def all_restaurants_handler():
-  #YOUR CODE HERE
-    
-@app.route('/restaurants/<int:id>', methods = ['GET','PUT', 'DELETE'])
+    # YOUR CODE HERE
+
+    if request.method == 'GET':
+
+        restaurants = session.query(Restaurant).all()
+        return jsonify(restaurants=[i.serialize for i in restaurants])
+
+    elif request.method == 'POST':
+        # MAKE A NEW RESTAURANT AND STORE IT IN DATABASE
+        location = request.args.get('location', '')
+        mealType = request.args.get('mealType', '')
+        restaurant_info = findARestaurant(mealType, location)
+        if restaurant_info != "No Restaurants Found":
+            restaurant = Restaurant(restaurant_name=str(restaurant_info['name']),
+                                    restaurant_address=str(restaurant_info['address']),
+                                    restaurant_image=restaurant_info['image'])
+            #restaurant = Restaurant(restaurant_name=unicode(restaurant_info['name']),
+            #                        restaurant_address=unicode(restaurant_info['address']),
+            #                        restaurant_image=restaurant_info['image'])
+            session.add(restaurant)
+            session.commit()
+            return jsonify(restaurant=restaurant.serialize)
+        else:
+            return jsonify({"error": "No Restaurants Found for %s in %s" % (mealType, location)})
+
+
+@app.route('/restaurants/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def restaurant_handler(id):
-  #YOUR CODE HERE
+    # YOUR CODE HERE
+    restaurant = session.query(Restaurant).filter_by(id=id).one()
+    if request.method == 'GET':
+        # RETURN A SPECIFIC RESTAURANT
+        return jsonify(restaurant=restaurant.serialize)
+    elif request.method == 'PUT':
+        # UPDATE A SPECIFIC RESTAURANT
+        address = request.args.get('address')
+        image = request.args.get('image')
+        name = request.args.get('name')
+        if address:
+            restaurant.restaurant_address = address
+        if image:
+            restaurant.restaurant_image = image
+        if name:
+            restaurant.restaurant_name = name
+        session.commit()
+        return jsonify(restaurant=restaurant.serialize)
+
+    elif request.method == 'DELETE':
+        # DELETE A SPECFIC RESTAURANT
+        session.delete(restaurant)
+        session.commit()
+        return "Restaurant Deleted"
+
 
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
-
-
-  
